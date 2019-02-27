fe8e77e4fee33b83d25b1aa57258fadea591c475
pelumzeny <pelumzeny10@gmail.com>
2019-02-27 11:38:40 +0100

    created the application

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cat_age: UITextField!
+    @IBOutlet weak var display_age: UILabel!
+    
+    @IBAction func GetAge(_ sender: Any) {
+        if let cat_human_age = cat_age.text {
+            if let intage = Int(cat_human_age) {
+                let real_age = intage * 7
+                display_age.text = "Your cat is " + String(real_age) + " cat years old"
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
